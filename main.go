06d886df03e77ba6e7a379b3535f6086a6b86871
Andrii Zhezhel <andrii.zhezhel@gmail.com>
2019-07-16 01:39:53 +0200

    Add graceful shutdown

--- main.go
+++ main.go
@@ -4,31 +4,41 @@ import (
 	"context"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/go-chi/chi"
 	"github.com/urfave/cli"
 )
 
 func main() {
 
 	app := cli.NewApp()
-	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:   "d,database",
-			EnvVar: "DATABASE_URL",
-			Value:  "sqlite3://:memory:",
-		},
-		cli.IntFlag{
-			Name:   "p,port",
-			EnvVar: "PORT",
-			Value:  8080,
-		},
-	}
+	app.Author = "Andrii Zhezhel"
+	app.Email = "andrii.zhezhel@gmail.com"
+	app.Name = "fetcher"
+	app.Version = "0.1.0"
+
 	app.Action = func(c *cli.Context) error {
+		var stop = make(chan os.Signal)
+
+		signal.Notify(stop, syscall.SIGTERM)
+		signal.Notify(stop, syscall.SIGINT)
+
 		ctx, cancel := context.WithCancel(context.Background())
 		defer cancel()
-		pool := NewPool(ctx)
-		r := getRouter(pool)
-		if err := http.ListenAndServe(":8080", r); err != nil {
+
+		r := getRouter(NewPool(ctx))
+		srv := http.Server{Addr: ":8080", Handler: chi.ServerBaseContext(ctx, r)}
+
+		go func() {
+			<-stop
+			srv.Shutdown(ctx)
+
+			cancel()
+		}()
+
+		if err := srv.ListenAndServe(); err != nil {
 			return cli.NewExitError(err, 1)
 		}
 
